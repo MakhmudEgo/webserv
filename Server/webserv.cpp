f09bfabbab5181e75551a636d2dbb9c9e67db013
MakhmudEgo <MakhmudEgo@yandex.ru>
2021-02-25 09:57:48 +0300

    add: small answer from server

--- Server/webserv.cpp
+++ Server/webserv.cpp
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <sstream>
 
 int main()
 {
@@ -35,6 +36,24 @@ int main()
 		std::cout << "Failed to listen on socket. errno: " << errno << std::endl;
 		exit(EXIT_FAILURE);
 	}
+	char msg[] = "hello from server\n";
+
+	std::stringstream response_body;
+	response_body << "<title>Test C++ HTTP Server</title>\n"
+				  << "<h1>Test page</h1>\n"
+				  << "<p>This is body of the test page...</p>\n"
+				  << "<h2>Request headers</h2>\n"
+				  << "<pre>" << msg << "</pre>\n"
+				  << "<em><small>Test C++ Http Server</small></em>\n";
+
+	// Формируем весь ответ вместе с заголовками
+	std::stringstream response;
+	response << "HTTP/1.1 200 OK\r\n"
+			 << "Version: HTTP/1.1\r\n"
+			 << "Content-Type: text/html; charset=utf-8\r\n"
+			 << "Content-Length: " << response_body.str().length()
+			 << "\r\n\r\n"
+			 << response_body.str();
 
 	// Grab a connection from the queue
 	int addrlen = sizeof(sockaddr);
@@ -55,9 +74,8 @@ int main()
 		std::cout << "The message was: " << buffer;
 		bzero(buffer, 2048);
 		// Send a message to the connection
-		char msg[] = "hello from server\n";
 		// write
-		send(connection, msg, strlen(msg), 0);
+		send(connection, response.str().c_str(), response.str().length(), 0);
 
 		// Close the connections
 		close(connection);
